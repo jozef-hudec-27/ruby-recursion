5468753ecfc46d617a1979f84f3a4819d379dbd3
Jozef Hudec <jojohudec27@gmail.com>
2022-08-25 10:06:10 +0200

    Implement basic merge sort

--- recursion.rb
+++ recursion.rb
@@ -16,3 +16,42 @@ def fibonacci_recursive(n)
   prev = fibonacci_recursive(n - 1)
   prev.concat([prev[-2] + prev[-1]])
 end
+
+def merge_sorted_arrays(arr1, arr2)
+  sorted = []
+  left = 0
+  right = 0
+
+  while left < arr1.length && right < arr2.length
+    if arr1[left] < arr2[right]
+      sorted << arr1[left]
+      left += 1
+    else
+      sorted << arr2[right]
+      right += 1
+    end
+  end
+
+  while left < arr1.length
+    sorted << arr1[left]
+    left += 1
+  end
+
+  while right < arr2.length
+    sorted << arr2[right]
+    right += 1
+  end
+
+  sorted
+end
+
+def merge_sort(arr)
+  n = arr.length
+  return arr if n <= 1
+
+  left_half_sorted = merge_sort(arr[0...n / 2])
+  right_half_sorted = merge_sort(arr[n / 2..])
+  merge_sorted_arrays(left_half_sorted, right_half_sorted)
+end
+
+p merge_sort([1, 7, 4, 1, 0, 58, 96, 32, 14])
